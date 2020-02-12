ad9277b825565db81dcf08f9b60af261f362aaff
david <sservicetk@gmail.com>
2020-02-12 16:54:14 -0500

    response rutas

---
+++ network/response.js
@@ -0,0 +1,8 @@
+exports.success = function (req, res, message) {
+    res.send(message);
+}
+
+exports.error = function (req, res) {
+    
+    
+}
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const response = require('./network/response');
 const router = express.Router();
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router);
 
 router.get('/', function (req, res){
-    res.send('hola');
+    res.header({
+        "custom-header":"personalizado",
+    });
+
+    response.success(req, res, 'mensajes')
 });
 
-/* app.use('/',function(req, res) {
-    res.send('hola');
-}); */
-
+app.use('/app',express.static('public'));
 app.listen(3000);
 
 console.log('corriendo');
